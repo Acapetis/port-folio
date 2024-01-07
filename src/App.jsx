3d582bb9a3a93084c0f49e57e1cfa4fa0db5ff6a
pandin lailert <pandinlailert@pandins-MacBook-Air.local>
2024-01-07 21:06:27 +0700

    add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import './App.css'
 import Navbar from "./components/Navbar"
 import Portfolio from "./components/Portfolio"
+import Home from "./components/Home"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='portfolio' element={<Portfolio/>} />
+          <Route path='home' element={<Home/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import App from "../App";
+import { Link } from "react-router-dom";
+function Home() {
+    return (
+
+        
+        <div>
+            <div class="mx-auto m-15">
+
+                <img class="mx-auto m-15" src="../assets/Pandin.jpg" alt="resume"/>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Home
